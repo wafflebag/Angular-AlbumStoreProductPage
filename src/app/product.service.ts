630e06f89090a1e5967f3681cdf45ec6089742e6
wafflebag <>
2019-07-20 18:12:16 -0800

    Competing part 3 v 0.2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 //import  {Http, Response } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/http';
+//import { HttpClient } from '@angular/common/http';
 
 //import 'rxja/add/operator/map';
 
@@ -12,11 +12,11 @@ import { HttpClient } from '@angular/common/http';
 export class ProductService {
   private _albumUrl = '../asset/album.json';
 
-  //constructor(private _http: Http) { }
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _http: Http) { }
+  //constructor(private _httpClient: HttpClient) { }
 
   getAlbum(id: number) {
-    //return this._http.get(this._albumUrl).map((response) => response.json());
-    return this._httpClient.get(this._albumUrl);
+    return this._http.get(this._albumUrl).map((response) => response.json());
+    //return this._httpClient.get(this._albumUrl);
   }
 }
